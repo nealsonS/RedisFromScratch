2aab722a9ddc11b34933d2a4e3c51afde519caa1
Nealson <nealsonsetiawan03@gmail.com>
2026-01-28 22:10:48 -0800

    added socket writing and reading in loops function

---
+++ src/io.c
@@ -0,0 +1,52 @@
+#include <assert.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+// func to read in a loop
+// bcos incoming requests can be less than buffer size
+// because tcp might not give all request data at once
+// but it's split into packets
+// so EOF might not even reach in one request
+int32_t read_full(int fd, char* buf, size_t n) {
+  while (n > 0){
+
+    // read n bytes into the buffer size
+    // returns how many bytes were read
+    ssize_t rv = read(fd, buf, n);
+    
+    // if EOF
+    if (rv <= 0) {
+      return -1; //error or unexpected EOF
+    }
+
+    // check that we only read n bytes (or less)
+    assert((size_t)rv <= n);
+    
+    // read only what's left
+    n -= (size_t)rv;
+    // seek the buffer pointer by how much was read
+    buf += rv;
+  }
+  return 0;
+}
+
+int32_t write_full(int fd, char* buf, size_t n){
+  while(n>0){
+  // returns how many bytes was written
+  ssize_t rv = write(fd, buf, n);
+
+  if (rv <= 0){
+    return -1;
+  }
+
+  assert((size_t)rv <= n);
+
+  // write only what's left
+  n -= (size_t)rv;
+
+  // seek the buffer pointer by how much was written
+  // to only write what's left of the bufer
+  buf += rv;
+  }
+  return 0;
+}
---
+++ src/io.h
@@ -0,0 +1,4 @@
+#include <sys/types.h>
+
+int32_t read_full(int fd, char *buf, size_t n);
+int32_t write_full(int fd, const char *buf, size_t n);
